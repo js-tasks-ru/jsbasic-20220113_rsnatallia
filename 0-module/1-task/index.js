78784428c06b0c8d0fff989702a6044195207f21
rsnatallia <97648629+rsnatallia@users.noreply.github.com>
2022-01-24 15:40:37 -0500

    Update index.js
    new update

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,14 +1,14 @@
 function sum (m, n) {
     return m + n;}
-undefined
 let result = sum(1, 2)
-undefined
 alert( result );
 }
+
+
+
+
 function sum (m, n) {
     return m + n;}
-undefined
 let result = sum(2, 5)
-undefined
 alert( result );
 }
