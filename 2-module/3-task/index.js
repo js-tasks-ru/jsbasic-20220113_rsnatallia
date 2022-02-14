3996c0d19520c5ea2e338fcf458d8b8e358847a6
Natallia Rocis <rsnatallia@yahoo.com>
2022-02-14 18:35:45 -0500

    Создайте калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,14 @@
 let calculator = {
-  // ваш код
-};
+  a:0,
+  b:0,
+  read: function (a1,b1) { this.a=a1; this.b=b1},
+  sum: function () {return this.a+this.b},
+  mul: function () {return this.a*this.b}
+   }
+
+  calculator.read(3, 5);
+  console.log(calculator.sum()); // 8
+  console.log(calculator.mul()); // 15
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
