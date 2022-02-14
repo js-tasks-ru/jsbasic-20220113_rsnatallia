a3494a495751e8f00ace8bf87e76fbf4b09ae608
Natallia Rocis <rsnatallia@yahoo.com>
2022-02-14 18:31:02 -0500

    Определите пуст ли объект

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
 function isEmpty(obj) {
-  // ваш код...
+  for ( x in obj){
+      return false
+  }
+  return true
 }
+let schedule = {};
+
+alert( isEmpty(schedule) );
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) );
