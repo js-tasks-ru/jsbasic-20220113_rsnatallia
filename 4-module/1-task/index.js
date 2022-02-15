8ae91a684fe2232f3dc75a9e7e42f90efad6aa9b
Natallia Rocis <rsnatallia@yahoo.com>
2022-02-15 16:31:30 -0500

    Найти min/max в строке

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,17 @@
-function makeFriendsList(friends) {
-  // ваш код...
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+function namify(users) {
+  let names=[]
+  for (el of users) {
+    names.push(el.name)
+  }
+  return names
 }
+
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
+
+console.log (names);
