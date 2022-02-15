08c104c927ddd7d7310375057b610c07904c7a6e
Natallia Rocis <rsnatallia@yahoo.com>
2022-02-15 16:26:24 -0500

    Функция showSalary(users, age)

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,18 @@
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
 function getMinMax(str) {
-  // ваш код...
+    let newarr = str.split(' ');
+    let min=parseFloat(newarr[0]);
+    let max=parseFloat(newarr[0]);
+    let result = {
+    min: min,
+    max: max
 }
+ for (el of newarr){
+    if ( parseFloat(el)<min) min=el;
+    if ( parseFloat(el)>max) max=el;
+ }
+ result.min=min
+ result.max=max
+ return result
+}
+console.log(getMinMax(inputData));
